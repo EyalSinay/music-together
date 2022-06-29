77970e46a74396872e96414d510ceb2eb745684b
Ofer Tauber <ofertauber@gmail.com>
2022-06-29 16:48:38 +0300

    Mogo is connected

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const route = require('./src/route');
 const path = require('path');
 require('./mongodb/mongoose');
 
-const {runPuppeteer} = require('./schedule.js');
+const { runPuppeteer } = require('./schedule.js');
 // runPuppeteer();
 
 const PORT = process.env.PORT || 5000;
@@ -22,8 +22,6 @@ app.listen(PORT, (req, res) => {
   void req, res;
   console.log('Listen to port: ' + PORT);
 });
-
-
 
 // every month: 0 0 1 * *
 const job = schedule.scheduleJob('0 0 1 * *', function () {
