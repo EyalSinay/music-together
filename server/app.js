5351f34a2ad39949939d454c3938e947ef02b0a3
EyalSinay <eyalsinay@gmail.com>
2022-06-29 17:04:05 +0300

    routs9

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const schedule = require('node-schedule');
 const route = require('./src/route');
 const path = require('path');
-require('./mongodb/mongoose');
+// require('./mongodb/mongoose');
 
 const {runPuppeteer} = require('./schedule.js');
 // runPuppeteer();
@@ -18,14 +18,12 @@ app.use(express.json());
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 app.use('/', route);
 
-app.listen(PORT, (req, res) => {
-  void req, res;
-  console.log('Listen to port: ' + PORT);
-});
-
-
-
 // every month: 0 0 1 * *
 const job = schedule.scheduleJob('0 0 1 * *', function () {
   console.log('The answer to life, the universe, and everything!');
+});
+
+app.listen(PORT, (req, res) => {
+  void req, res;
+  console.log('Listen to port: ' + PORT);
 });
